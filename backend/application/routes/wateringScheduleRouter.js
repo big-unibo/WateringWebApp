29f774f551c362238b9417313c38fd758a854aa1
Alex Baiardi <alex.baiardi99@gmail.com>
2025-07-17 17:14:01 +0200

    feat: add API for watering calendar creation

--- backend/application/routes/wateringScheduleRouter.js
+++ backend/application/routes/wateringScheduleRouter.js
@@ -207,6 +207,99 @@ wateringScheduleRouter.post("/createWateringEvent", async (req, res) => {
 
 /**
  * @swagger
+ * /wateringSchedule/{refStructureName}/{companyName}/{fieldName}/{sectorName}/createCalendar:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Create watering events for a sector
+ *     description:  Crete watering events for a sector based on the watering parameters
+ *     tags: [Watering Schedule Operation]
+ *     parameters:
+ *       - in: path
+ *         name: refStructureName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The reference structure name
+ *       - in: path
+ *         name: companyName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The company name
+ *       - in: path
+ *         name: fieldName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The field name
+ *       - in: path
+ *         name: sectorName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The sector name
+ *       - in: query
+ *         name: dateFrom
+ *         required: true
+ *         description: The first date in wich watering events should be created
+ *         schema:
+ *           type: string
+ *           format: date
+ *       - in: query
+ *         name: dateTo
+ *         required: true
+ *         description: The last date in wich watering events should be created
+ *         schema:
+ *           type: string
+ *           format: date     
+ *     responses:
+ *       '200':
+ *         description: Calendar events created successfully
+ *       '400':
+ *         description: Invalid request.
+ *       '401':
+ *         description: Unauthorized request.
+ *       '403':
+ *         description: Authentication failed.
+ *       '500':
+ *         description: Error on create calendar events.
+ */
+wateringScheduleRouter.post("/:refStructureName/:companyName/:fieldName/:sectorName/createCalendar", async (req, res) => {
+    let user
+    try {
+        user = await authenticationService.validateJwt(req.headers.authorization);
+    } catch (error) {
+        console.log(error)
+        return res.status(403).json({ message: 'Authentication failed' });
+    }
+    const refStructureName = req.params.refStructureName;
+    const companyName = req.params.companyName;
+    const fieldName = req.params.fieldName;
+    const sectorName = req.params.sectorName;
+    const dateFrom = req.query.dateFrom;
+    const dateTo = req.query.dateTo;
+
+    if (!(await authorizationService.isUserAuthorizedByFieldAndId(user.userid, refStructureName, companyName, fieldName, sectorName, null, 'WA')))
+        return res.status(401).json({ message: 'Unauthorized request' });
+
+    if (!dateFrom || !dateTo || isNaN(new Date(dateFrom).getTime()) || isNaN(new Date(dateTo).getTime())) {
+        return res.status(400).json({ message: 'Invalid request, invalid params dateFrom and dateTo' });
+    }
+    try {
+        if( new Date(dateFrom) > new Date(dateTo)) {
+            return res.status(400).json({ message: 'Invalid request, dateFrom must be before dateTo' });
+        }
+
+        await wateringScheduleService.createWateringCalendar(refStructureName, companyName, fieldName, sectorName, dateFrom, dateTo, -1);
+        res.status(200).json({message: `Watering calendar create with success`});
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+
+/**
+ * @swagger
  * /wateringSchedule/{refStructureName}/{companyName}/{fieldName}/{sectorName}/endIrrigationSeason:
  *   post:
  *     security:
