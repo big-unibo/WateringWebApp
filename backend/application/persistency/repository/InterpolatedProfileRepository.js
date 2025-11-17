cfc1674a6f1108370f5da2f2cd4618e3d543d535
Michele Farneti <michele.farneti@studio.unibo.it>
2025-11-17 17:34:39 +0100

    Implemented getInterpolatedMeans query

--- backend/application/persistency/repository/InterpolatedProfileRepository.js
+++ backend/application/persistency/repository/InterpolatedProfileRepository.js
@@ -53,6 +53,54 @@ class InterpolatedProfileRepository {
         return results;
     }
 
+    async getInterpolatedMeans(thesisId, timeFilterFrom, timeFilterTo) {
+
+        const query = `
+            WITH validity_table AS (
+                SELECT DISTINCT
+                    tas.thesis_name,
+                    tas.device_id,
+                    tas.device_binning_id,
+                    tas.valid_from,
+                    tas.valid_to
+                FROM theses_all_signals tas
+                WHERE tas.device_type = :HUMIDITY_DEVICE_TYPE
+                AND tas.thesis_id = :thesisId
+            )
+            SELECT DISTINCT
+                v.thesis_name AS "thesisName",
+                v.device_id AS "deviceId",
+                v.device_binning_id AS "binningId",
+                ip.x AS "x",
+                ip.y AS "y",
+                ip.z AS "z",
+                AVG(ip.value * -1)::numeric AS mean, 
+                STDDEV(ip.value) ::numeric AS std
+            FROM validity_table v
+            JOIN interpolated_profiles ip 
+                ON ip.grid_id = v.device_id
+                AND ip.timestamp BETWEEN 
+                    GREATEST(v.valid_from, :timeFilterFrom)
+                    AND LEAST(COALESCE(v.valid_to, 'infinity'), :timeFilterTo)
+            GROUP BY v.thesis_name, v.device_id , v.device_binning_id, ip.x, ip.y, ip.z
+            ORDER BY ip.timestamp ASC;
+        `;
+
+
+        const results = await this.sequelize.query(query, {
+            replacements: {
+                timeFilterFrom,
+                timeFilterTo,
+                thesisId,
+                HUMIDITY_DEVICE_TYPE
+            },
+            type: QueryTypes.SELECT
+        });  
+
+        return results;
+    }
+
+
     async findLastInterpolationTimestamp(thesisId, timestampFrom, timestampTo)
     {
         const query = `
