fca80b9e527ab14657697011b0954b1ce178fd2d
Alex Baiardi <alex.baiardi99@gmail.com>
2025-11-28 16:05:52 +0100

    refactor: simplified get Result query

--- backend/application/persistency/repository/ThesesAllSignalsRepository.js
+++ backend/application/persistency/repository/ThesesAllSignalsRepository.js
@@ -14,34 +14,20 @@ class ThesesAllSignalsRepository {
         signalTypes,
         timeFilterFrom,
         timeFilterTo,
-        aggregationType = 'SUM',
+        aggregationType,
         aggregationPeriod
     ) {
+        const col = "value";
 
+        const aggregationFunctions = {
+            SUM:  `SUM(${col})`,
+            AVG:  `AVG(${col})`,
+            MIN:  `MIN(${col})`,
+            MAX:  `MAX(${col})`,
+            MED:  `percentile_cont(0.5) WITHIN GROUP (ORDER BY ${col})`,
+        };
 
-        // Determina la stringa SQL da passare a getResults in base al tipo di aggregazione
-        let sqlAggregation;
-        switch (aggregationType.toUpperCase()) {
-            case 'SUM':
-                sqlAggregation = 'SUM(vm.value)';
-                break;
-            case 'AVG':
-                sqlAggregation = 'AVG(vm.value)';
-                break;
-            case 'MIN':
-                sqlAggregation = 'MIN(vm.value)';
-                break;
-            case 'MAX':
-                sqlAggregation = 'MAX(vm.value)';
-                break;
-            case 'MED':
-                sqlAggregation = 'percentile_cont(0.5) WITHIN GROUP (ORDER BY vm.\"value\")';
-                break;
-            default:
-                sqlAggregation = 'AVG(vm.value)';
-                break;
-        }
-
+        const sqlAggregation = aggregationFunctions[aggregationType?.toUpperCase()] || aggregationFunctions.AVG
         const results = await this.getResults(thesisId, signalTypes, timeFilterFrom, timeFilterTo, sqlAggregation, aggregationPeriod);
 
         return Array.isArray(results) ? results : [];
@@ -50,69 +36,10 @@ class ThesesAllSignalsRepository {
 
     async getResults(thesisId, signalTypes, timeFilterFrom, timeFilterTo, sqlAggregation, aggregationPeriod) {
         const query = `
-            WITH valid_measurements_table AS (
-                SELECT DISTINCT
-                    tas.thesis_name,
-                    tas.device_id,
-                    tas.signal_id,
-                    tas.signal_description,
-                    tas.signal_type,
-                    tas.signal_type_description,
-                    tas.x,
-                    tas.y,
-                    tas.z,
-                    tas.virtual,
-                    tas.unit,
-                    m.computed,
-                    m.value,
-                    m.raw_value,
-                    m.timestamp
-                FROM theses_all_signals tas
-                LEFT JOIN measurements m
-                    ON m.signal_id = tas.signal_id
-                AND m.timestamp BETWEEN 
-                    GREATEST(tas.valid_from, :timeFilterFrom)
-                    AND LEAST(COALESCE(tas.valid_to, 'infinity'), :timeFilterTo)
-                WHERE tas.signal_type = ANY(ARRAY[:signalTypes])
-                AND tas.thesis_id = :thesisId
-            ),
-            aggregated AS (
-                SELECT
-                    vm.thesis_name,
-                    vm.device_id,
-                    vm.signal_id,
-                    vm.signal_description,
-                    vm.signal_type,
-                    vm.signal_type_description,
-                    vm.x,
-                    vm.y,
-                    vm.z,
-                    vm.virtual,
-                    vm.unit,
-                    vm.computed,
-                    ${sqlAggregation} AS value,
-                    ARRAY_AGG(vm.raw_value) FILTER (WHERE vm.raw_value IS NOT NULL) AS raw_value,
-                    ROUND(vm.timestamp::NUMERIC / :aggregationPeriod) * :aggregationPeriod AS timestamp
-                FROM valid_measurements_table vm
-                GROUP BY
-                    vm.thesis_name,
-                    vm.device_id,
-                    vm.signal_id,
-                    vm.signal_description,
-                    vm.signal_type,
-                    vm.signal_type_description,
-                    vm.x,
-                    vm.y,
-                    vm.z,
-                    vm.virtual,
-                    vm.unit,
-                    vm.computed,
-                    ROUND(vm.timestamp::NUMERIC / :aggregationPeriod) * :aggregationPeriod
-            )
             SELECT
                 thesis_name AS "thesisName",
                 device_id AS "deviceId",
-                signal_id AS "signalId",
+                tas.signal_id AS "signalId",
                 signal_description AS "signalDescription",
                 signal_type AS "signalType",
                 signal_type_description AS "signalTypeDescription",
@@ -120,9 +47,30 @@ class ThesesAllSignalsRepository {
                 virtual,
                 unit,
                 computed,
-                timestamp,
-                COALESCE(to_jsonb(value), to_jsonb(raw_value)) AS "value"
-            FROM aggregated
+                ROUND(timestamp::NUMERIC / :aggregationPeriod) * :aggregationPeriod AS "timestamp",
+                COALESCE(to_jsonb(${sqlAggregation}), to_jsonb(ARRAY_AGG(raw_value) FILTER (WHERE raw_value IS NOT NULL))) AS "value"
+            FROM theses_all_signals tas
+            LEFT JOIN measurements m
+                ON m.signal_id = tas.signal_id
+            AND m.timestamp BETWEEN 
+                GREATEST(tas.valid_from, :timeFilterFrom)
+                AND LEAST(COALESCE(tas.valid_to, 'infinity'), :timeFilterTo)
+            WHERE tas.signal_type = ANY(ARRAY[:signalTypes])
+            AND tas.thesis_id = :thesisId
+            GROUP BY
+                thesis_name,
+                device_id,
+                tas.signal_id,
+                signal_description,
+                signal_type,
+                signal_type_description,
+                x,
+                y,
+                z,
+                virtual,
+                unit,
+                computed,
+                ROUND(timestamp::NUMERIC / :aggregationPeriod) * :aggregationPeriod
             ORDER BY timestamp ASC;
             `;
 
